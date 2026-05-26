f6bbaeaf5e527dfa539847d1cedc7d535fff3779
Catherine <cath.samad1@gmail.com>
2026-05-27 00:06:02 +0400

    Add constants

---
+++ src/constants.py
@@ -0,0 +1,24 @@
+BUILTINS = {
+    'print': print
+}
+
+"""Rendering constants"""
+# Matrix brackets
+LEFT_TOP = '⎡'
+LEFT_MIDDLE = '⎢'
+LEFT_BOTTOM = '⎣'
+
+RIGHT_TOP = '⎤'
+RIGHT_MIDDLE = '⎥'
+RIGHT_BOTTOM = '⎦'
+
+# Row vector brackets
+ROW_VECTOR_LEFT = '['
+ROW_VECTOR_RIGHT = ']'
+
+# Formatting
+COLUMN_SEPARATOR = '  '
+ROW_SEPARATOR = '\n'
+
+# Optional spacing configuration
+MIN_COLUMN_WIDTH = 1
